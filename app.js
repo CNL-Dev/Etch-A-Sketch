8b649545bac93ab27493daf6ac74e2d8b915809c
Caleb Lugo <cnl1211999@gmail.com>
2023-09-19 15:15:27 -0700

    Moved logic to appropiate functions

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const container = document.querySelector('.container');
 
-// Creates 256 divs that will serve as our canvas for the
-// etch-a-sketch
-for(let i = 0; i < 256; i++){
-    const sketchPart = document.createElement('div');
-    sketchPart.classList.add('sketch');
-    container.appendChild(sketchPart);
+// Creates the horizontal rows that will contain
+// some of our divs
+function createCanvasRows() {
+    for(let i = 0; i < 16; i++){
+        const containerRow = document.createElement('div');
+        containerRow.classList.add('container-row');
+        container.appendChild(containerRow);
+    }
+}
+
+
+// Creates the column divs that will serve as the 
+// canvas for the etch-a-sketch
+function createCanvasColumns() {
+    for(let i = 0; i < 256; i++){
+        const sketchPart = document.createElement('div');
+        sketchPart.classList.add('sketch');
+        container.appendChild(sketchPart);
+    }
 }
