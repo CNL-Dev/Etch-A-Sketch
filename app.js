5a0c49da32aa3eee47dfdb28c7594f3ce07923d1
Caleb Lugo <cnl1211999@gmail.com>
2023-09-21 16:33:59 -0700

    Implemented logic to resize canvas with the resize button

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const container = document.querySelector('.canvas-container');
-const resizeGridButton = document.querySelector('.resize-grid-button');
+const canvasContainer = document.querySelector('.canvas-container');
+const resizeGridButton = document.querySelector('.resize-canvas-button');
 
 // Creates the horizontal rows that will contain
 // some of our divs
 function createCanvasRows(gridSize) {
-    for(let i = 0; i < gridSize; i++){
-        const containerRow = document.createElement('div');
-        containerRow.classList.add('canvas-container-row');
-        container.appendChild(containerRow);
+    for(let i = 0; i < gridSize; i++) {
+        const canvasContainerRow = document.createElement('div');
+        canvasContainerRow.classList.add('canvas-container-row');
+        canvasContainer.appendChild(canvasContainerRow);
     }
 }
 
 // Creates the column divs that will serve as the 
 // canvas for the etch-a-sketch
 function createCanvasColumns(parent, gridSize) {
-    for(let i = 0; i < gridSize; i++){
+    for(let i = 0; i < gridSize; i++) {
         const sketchPart = document.createElement('div');
         sketchPart.classList.add('canvas');
         parent.appendChild(sketchPart);
@@ -24,7 +24,7 @@ function createCanvasColumns(parent, gridSize) {
 // Generates the canvas
 function generateCanvas(gridSize) {
     // We can generate a grid size between 1 and 100
-    if(gridSize > 0 && gridSize < 101){
+    if(gridSize > 0 && gridSize < 101) {
         createCanvasRows(gridSize);
     
         const canvasRows = document.querySelectorAll('.canvas-container-row');
@@ -33,12 +33,19 @@ function generateCanvas(gridSize) {
     }   
 }
 
+// Removes the canvas
+function removeCanvas() {
+    while (canvasContainer.hasChildNodes()) {
+        canvasContainer.removeChild(canvasContainer.firstChild);
+    }
+}
+
 // Fills in a canvas spot when the mouse 
 // is detected
 function onCanvasHover() {
     const canvas = document.querySelectorAll('.canvas');
 
-    for(let i = 0; i < canvas.length; i++){
+    for(let i = 0; i < canvas.length; i++) {
         canvas[i].addEventListener('mousemove', (event) => {
             event.target.classList.add('black');
         });
@@ -47,3 +54,10 @@ function onCanvasHover() {
 
 // Default canvas for when the site is first loaded
 generateCanvas(16);
+
+resizeGridButton.addEventListener('click', () => {
+    let input = prompt("Input a number between 1 and 100: ");
+
+    removeCanvas();
+    generateCanvas(input);
+});
